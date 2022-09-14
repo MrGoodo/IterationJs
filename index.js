5bcd88c89e5b4c0e73efbf53906c51b5122f6e86
Nick Kandelaki <goodo.nick@gmail.com>
2022-09-14 18:12:55 +0300

    IterationJs

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 for (let i = 0; i < 101; i++) {
   console.log(i);
 }
+
+// цикл от 0 до 27 //
+for (let e = 0; e <= 27; e++) {
+  if (e == 0) {
+    console.log(e + " четное");
+  } else if (e % 2 == 0) {
+    console.log(e + " четное");
+  } else {
+    console.log(e + " нечетное");
+  }
+}
